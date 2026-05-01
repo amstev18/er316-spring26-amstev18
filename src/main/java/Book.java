a774eea29ba313521811870bbe4ec3ce9a16092c
aiden <amstev18@asu.edu>
2026-05-01 08:31:29 -0700

    refactor 2: removed the unnecessary available and referenceOnly fields in Book.java

--- src/main/java/Book.java
+++ src/main/java/Book.java
@@ -10,8 +10,6 @@ public class Book {
     private String title;
     private String author;
     private BookType type;
-    private boolean available;
-    private boolean referenceOnly;
     private int totalCopies;
     private int availableCopies;
 
@@ -38,15 +36,12 @@ public class Book {
         this.author = author;
         this.type = type;
         this.totalCopies = totalCopies;
-        this.referenceOnly = (type == BookType.REFERENCE);
 
         // Reference books never circulate - always unavailable for checkout
-        if (this.referenceOnly) {
+        if (isReferenceOnly()) {
             this.availableCopies = 0;
-            this.available = false;
         } else {
             this.availableCopies = totalCopies;
-            this.available = true;
         }
     }
 
@@ -72,7 +67,7 @@ public class Book {
     }
 
     public boolean isReferenceOnly() {
-        return referenceOnly;
+        return type == BookType.REFERENCE;
     }
 
     public int getTotalCopies() {
@@ -86,7 +81,6 @@ public class Book {
     // Setters
     public void setAvailableCopies(int copies) {
         this.availableCopies = copies;
-        this.available = (copies > 0);
     }
 
     /**
@@ -96,7 +90,6 @@ public class Book {
         if (availableCopies > 0) {
             availableCopies--;
         }
-        this.available = (availableCopies > 0);
     }
 
     /**
@@ -106,16 +99,10 @@ public class Book {
         if (availableCopies < 100) {
             availableCopies++;
         }
-        this.available = (availableCopies > 0);
     }
 
     public void resetAvailability() {
         this.availableCopies = this.totalCopies;
-        this.available = true;
-    }
-
-    public boolean checkAvailability() {
-        return this.available;
     }
 
     @Override
